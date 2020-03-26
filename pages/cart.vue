2b2f3c43ba93391a1dca4d3edbefc3da611389b7
aikazm <zakiamelia@gmail.com>
2020-03-26 22:01:55 +0700

    created cart and checkout pages

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,23 +1,88 @@
 <template>
-  <div>
+  <div class="cart mb-5">
     <b-container>
-      <h1 class="text-center">My Shopping Cart</h1>
-      <b-table striped hover :items="items"></b-table>
+      <h1 class="text-center mb-4">My Shopping Cart</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Product Details</th>
+            <th>Price</th>
+            <th>Qty</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <figure class="figure">
+                <b-img src="@/assets/nivea-1.jpg" class="thumbnail-product mr-3" />
+                <h6>NIVEA</h6>
+                <figcaption class="figure-caption">Double Eye Make Up Remover</figcaption>
+              </figure>
+            </td>
+            <td>Rp 20.000</td>
+            <td>
+              <div>
+                <b-input-group size="sm">
+                  <b-input-group-prepend>
+                    <b-button>
+                      <font-awesome-icon :icon="['fas', 'minus']" />
+                    </b-button>
+                  </b-input-group-prepend>
+
+                  <b-form-input class="qty"></b-form-input>
+
+                  <b-input-group-append>
+                    <b-button>
+                      <font-awesome-icon :icon="['fas', 'plus']" />
+                    </b-button>
+                  </b-input-group-append>
+                </b-input-group>
+              </div>
+            </td>
+            <td>Rp 20.000</td>
+          </tr>
+        </tbody>
+      </table>
+      <b-row>
+        <b-col>
+          <b-button>
+            <font-awesome-icon :icon="['fas', 'arrow-left']" class="mr-2" />Continue Shopping
+          </b-button>
+        </b-col>
+        <b-col cols="3">
+          <b-row>
+            <b-col>
+              <p>Sub Total</p>
+              <p>Total</p>
+            </b-col>
+            <b-col class="text-right">
+              <p>Rp 20.000</p>
+              <p>Rp 20.000</p>
+            </b-col>
+          </b-row>
+          <div class="d-flex justify-content-end">
+            <b-button>
+              Checkout
+              <font-awesome-icon :icon="['fas', 'arrow-right']" class="ml-2" />
+            </b-button>
+          </div>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      items: [
-        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
-        { age: 21, first_name: "Larsen", last_name: "Shaw" },
-        { age: 89, first_name: "Geneva", last_name: "Wilson" },
-        { age: 38, first_name: "Jami", last_name: "Carney" }
-      ]
-    };
-  }
-};
-</script>
+
+<style>
+.cart {
+  margin-top: 120px;
+}
+.thumbnail-product {
+  height: 60px;
+  float: left;
+}
+.qty {
+  width: 5px;
+}
+</style>
---
+++ pages/checkout.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="checkout mb-5">
+    <b-container>
+      <b-row>
+        <b-col>
+          <div class="shipping-address">
+            <b-row>
+              <b-col>
+                <h4 class="mb-4">Shipping Address</h4>
+                <b-form>
+                  <b-row>
+                    <b-col>
+                      <b-form-group id="input-group-2" label="First Name*" label-for="input-2">
+                        <b-form-input id="input-2" required></b-form-input>
+                      </b-form-group>
+                    </b-col>
+                    <b-col>
+                      <b-form-group id="input-group-2" label="Last Name*" label-for="input-2">
+                        <b-form-input id="input-2" required></b-form-input>
+                      </b-form-group>
+                    </b-col>
+                  </b-row>
+                  <b-form-group id="input-group-2" label="Full Address*" label-for="input-2">
+                    <b-form-input id="input-2" required></b-form-input>
+                  </b-form-group>
+                  <b-form-group id="input-group-2" label="City*" label-for="input-2">
+                    <b-form-input id="input-2" required></b-form-input>
+                  </b-form-group>
+                  <b-form-group id="input-group-2" label="Phone Number*" label-for="input-2">
+                    <b-form-input id="input-2" required></b-form-input>
+                  </b-form-group>
+                </b-form>
+              </b-col>
+            </b-row>
+          </div>
+        </b-col>
+        <b-col cols="5">
+          <div class="order-summary">
+            <h4 class="mb-4">Order Summary</h4>
+            <b-row>
+              <b-col>
+                <figure class="figure">
+                  <b-img src="@/assets/nivea-1.jpg" class="thumbnail-product mr-3" />
+                  <h6>NIVEA</h6>
+                  <figcaption class="figure-caption">Double Eye Make Up Remover</figcaption>
+                </figure>
+              </b-col>
+              <b-col class="text-right">
+                <p>Qty: 1</p>
+                <p>Rp. 20.000</p>
+              </b-col>
+            </b-row>
+            <b-row>
+              <b-col>
+                <p>Sub Total</p>
+                <p>Discount</p>
+                <p>Shipping</p>
+                <p>Total</p>
+              </b-col>
+              <b-col class="text-right">
+                <p>Rp 20.000</p>
+                <p>-</p>
+                <p>-</p>
+                <p>Rp 20.000</p>
+              </b-col>
+            </b-row>
+          </div>
+          <div class="mt-4 text-right">
+            <b-button>Proceed To Payment</b-button>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<style>
+.checkout {
+  margin-top: 120px;
+}
+.thumbnail-product {
+  height: 60px;
+  float: left;
+}
+.order-summary,
+.shipping-address {
+  border: 2px solid #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+}
+</style>
